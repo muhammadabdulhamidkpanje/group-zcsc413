463817962141152c01720f170a34264d555c30a6
Makpanje <kpanje005@gmail.com>
2023-06-02 07:35:30 +0100

    a first push to git-up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import Header from './header';
+import Homepage from './homepage';
+import Assignment from './Asssignment';
+import Footer from './footer/footer';
+import {   Routes, Route, BrowserRouter,} from 'react-router-dom';
 import './App.css';
+import AboutUs from './about-us';
+import ContactUS from './contact-us';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Header />
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path="/about-us" element={<AboutUs/>} />
+      <Route path="/contact-us" element={<ContactUS/>} />
+      <Route path="/assignment" element={<Assignment />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
     </div>
   );
 }
